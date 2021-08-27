73b6e263a7fa1ed087dffa812caaf2aeeda671da
Vladyslav Novytskyi <48761198+VNovytskyi@users.noreply.github.com>
2021-08-27 17:32:12 +0300

    Update crc.h

--- Source/crc.h
+++ Source/crc.h
@@ -1,17 +1,13 @@
 #ifndef CRC_H
 #define CRC_H
 
+#include <stdint.h>
+
 #ifdef STM8S103
 	#include "stm8s.h"
 #endif
 
-#include <stdint.h>
-
 #define TABLE_CRC
-
-#ifdef TABLE_CRC
-extern const uint16_t crc16_table[256];
-#endif
 
 uint16_t get_crc(const uint8_t *data, const uint16_t length);
 void get_crc_by_byte(uint16_t *crc_value, const uint8_t byte);
